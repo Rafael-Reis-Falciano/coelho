68ce371a415612878d640f134a303740d52c11f3
ProfaTatianeTobias <93044628+ProfaTatianeTobias@users.noreply.github.com>
2022-02-15 18:44:34 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,9 +12,11 @@ let world;
 var ground;
 
 var corda, bola, coelho;
-var fundoImg, frutaImg, coelhoImg, piscarImg, comerImg, bravoImg;
+var fundoImg, frutaImg, coelhoImg, piscarImg, comerImg, tristeImg;
 
-var botao;
+var somFundo, somTriste, somAr, somComendo, somCorte;
+
+var botao, botaoSom;
 
 var novoLink;
 
@@ -24,12 +26,18 @@ function preload(){
   coelhoImg = loadImage("Rabbit-01.png");
   piscarImg = loadAnimation("blink_1.png", "blink_2.png", "blink_3.png");
   comerImg = loadAnimation("eat_0.png", "eat_1.png", "eat_2.png", "eat_3.png", "eat_4.png" );
-  bravoImg = loadAnimation("sad_1.png", "sad_2.png", "sad_3.png");
+  tristeImg = loadAnimation("sad_1.png", "sad_2.png", "sad_3.png");
 
   piscarImg.playing = true;
   comerImg.playing = true;
   piscarImg.looping = true;
   comerImg.looping = false;
+
+  somFundo = loadSound("sound1.mp3");
+  somTriste = loadSound("sad.wav");
+  somCorte = loadSound("rope_cut.mp3");
+  somComendo = loadSound("eating_sound.mp3");
+  somAr = loadSound("air.wav");
 
 }
 
@@ -37,13 +45,17 @@ function setup()
 {
   createCanvas(500,700);
   frameRate(80);
+
+  somFundo.play();
+  somFundo.setVolume(0.1);
+
   engine = Engine.create();
   world = engine.world;
   ground = new Ground(200,690,600,20);
 
   piscarImg.frameDelay = 30;
   comerImg.frameDelay = 30;
-  bravoImg.frameDelay = 5;
+  tristeImg.frameDelay = 5;
 
 
   coelho = createSprite(250,610,100,100);
@@ -52,7 +64,7 @@ function setup()
 
   coelho.addAnimation("piscando", piscarImg);
   coelho.addAnimation("comendo", comerImg);
-  coelho.addAnimation("bravo", bravoImg);
+  coelho.addAnimation("triste", tristeImg);
   coelho.changeAnimation("piscando");
 
   rectMode(CENTER);
@@ -71,10 +83,15 @@ function setup()
   novoLink = new link (corda,bola);
 
   botao = createImg("cut_button.png");
-  botao.position(150, 150);
-  botao.size(100, 100);
-
+  botao.position(70, 120);
+  botao.size(70, 70);
   botao.mouseClicked(cair);
+
+  botaoSom = createImg("mute.png");
+  botaoSom.position(width-120, 120);
+  botaoSom.size(70, 70);
+  botaoSom.mouseClicked(mutar);
+  
 }
 
 function draw() 
@@ -86,7 +103,20 @@ function draw()
   Engine.update(engine);
   
   corda.show();
+
+  if(bola!=null){
   image(frutaImg, bola.position.x, bola.position.y, 60, 60);
+  }
+
+  if(colidir(bola,coelho) === true){
+    coelho.changeAnimation("comendo");
+  }
+
+  if(colidir(bola,ground.body) === true){
+    coelho.changeAnimation("triste");
+  }
+
+
 
   drawSprites();
    
@@ -94,11 +124,35 @@ function draw()
 
 function cair()
 {
+  somCorte.play();
   corda.break();
   novoLink.cortar();
   novoLink = null;
 }
 
+function colidir(body,sprite){
+  if(body !== null){
+    var d = dist(body.position.x, body.position.y, sprite.position.x, sprite.position.y);
+    if(d<=60){
+      World.remove(engine.world,bola);
+      bola = null;
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+}
+
+function mutar(){
+  if(somFundo.isPlaying()){
+    somFundo.stop();
+  }
+  else{
+    somFundo.play();
+  }
+}
 
 
 
